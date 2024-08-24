4e9206f709d2d99386043ba9265fc232928b43bf
Ayoub Jama Khalid <buuyacamaajdilaakh@gmail.com>
2024-08-24 11:51:10 +0300

    my answers

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hello");
+function calcTip(bill) {
+    const tip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
+    const total = bill + tip;
+    console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${total}`);
+    return tip;
+}
+
+calcTip(275); 
